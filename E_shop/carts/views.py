4e5de14a7536c75d8282c3d95b85f8c6bd35ce16
mr_X <mr.Aleksandrsmith@gmail.com>
2023-07-07 15:37:54 +0300

    add cart

--- E_shop/carts/views.py
+++ E_shop/carts/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from store.models import Product
+from .models import Cart
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        request.session.create
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
 
 
 def cart(request):
